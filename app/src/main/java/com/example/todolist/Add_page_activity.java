075d5c0f8f53d3d7f635d1069c8417926ed358ae
Yaser <mohammadyaser1379@gmail.com>
2020-06-13 16:30:48 +0430

    azafe kardan e button bara add v raftan b safhe y add

---
+++ app/src/main/java/com/example/todolist/Add_page_activity.java
@@ -0,0 +1,14 @@
+package com.example.todolist;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Add_page_activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_do_list);
+    }
+}
