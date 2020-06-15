b3eb43613313f9fa5f723929dc73ba25115db3b7
Yaser <mohammadyaser1379@gmail.com>
2020-06-15 18:45:24 +0430

    sakht layout baray details

---
+++ app/src/main/java/com/example/todolist/Detail_Of_Task.java
@@ -0,0 +1,14 @@
+package com.example.todolist;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Detail_Of_Task extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_task);
+    }
+
+}
