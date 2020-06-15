964ca14648bc626841c8f7376f41f857aaa64c7f
Yaser <mohammadyaser1379@gmail.com>
2020-06-15 12:07:25 +0430

    ui e safhe y add dorost shod . vali haniz gereftan e matn va sakht shei az task mondeh

--- app/src/main/java/com/example/todolist/Add_page_activity.java
+++ app/src/main/java/com/example/todolist/Add_page_activity.java
@@ -1,14 +1,50 @@
 package com.example.todolist;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class Add_page_activity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_do_list);
+
+        final Intent intent = new Intent(this, MainActivity.class);
+        Button button = this.findViewById(R.id.add_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
+
+
+
 }
+
+
+
